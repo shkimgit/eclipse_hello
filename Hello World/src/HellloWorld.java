d2ea5cc2e8329a01102a23bc3422bafeb11146a3
shkim <shkim@38chungchun.com>
2016-05-25 16:42:49 +0900

    added print line

--- Hello World/src/HellloWorld.java
+++ Hello World/src/HellloWorld.java
@@ -3,5 +3,6 @@ public class HellloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("World's End Dancehall");
 	}
 }
