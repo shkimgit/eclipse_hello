ca8625bc865a9f69793daaa7186b6f9465958848
shkimgit <shkim4936@outlook.com>
2016-05-27 14:19:42 +0900

    add miku

--- Hello World/src/PushPrint.java
+++ Hello World/src/PushPrint.java
@@ -5,6 +5,7 @@ public class PushPrint {
 		// TODO Auto-generated method stub
 		System.out.println("Add Java class File push");
 		System.out.println("World with 42");
+		System.out.println("World with Miku");
 	}
 
 }
